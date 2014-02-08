033a4499b3baa69290a2e85a85fa82558d76ab63
Matthew Davies <daviesgeek@icloud.com>
2014-02-08 10:03:50 -0800

    Removed margin from body so that it centers correctly Added data-toggle=dropdown to all the dropdown links in the navbar (inc_top.tmpl) Added dropdown/link support for both desktop and mobile support (ui.js)  - On the desktop, hover shows the dropdowns, click goes to the link in the li  - On mobile, tap shows the dropdowns, another tap goes to the link in the li

--- gui/new/js/ui.js
+++ gui/new/js/ui.js
@@ -1,9 +1,26 @@
 (function($) {
 	$(function() {
 
-		 $('li.dropdown').hover(function(){
-		 		$(this).children('ul.dropdown-menu').toggle();
-		 });
+		// Variables for UI.js
+		var dropdown = 'ul.dropdown-menu';
+		var liDropdown = 'li.dropdown';
+		var linkLocation = 'a.dropdown-link';
+
+		// If the device is a touch screen device
+		if(!!('ontouchstart' in window)){
+			$(liDropdown).click(function(){
+				if ($(liDropdown).children(dropdown).is(':visible')) {
+					window.location = $(this).children(linkLocation).attr('href');
+				}				
+			})
+		}else{ // Else, use hover
+			$(liDropdown).hover(function(){
+				$(this).children(dropdown).toggle();
+			});
+			$(liDropdown).click(function(){
+				window.location = $(this).children(linkLocation).attr('href');
+			});
+		}
 
 	});
 })(jQuery);
